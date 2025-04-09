167a4f48b4f46e13b723ce6ebbe2c45841d2f2fb
Gabriel-Feang-Meli <gabriel.fangelo@mercadolivre.com>
2025-04-09 10:11:55 -0300

    empty branch

--- src/main/java/io/meli/melishop/factory/RecommendationStrategyFactory.java
+++
@@ -1,8 +0,0 @@
-package io.meli.melishop.factory;
-
-import io.meli.melishop.strategy.RecommendationStrategy;
-
-public abstract class RecommendationStrategyFactory {
-
-    protected abstract RecommendationStrategy createRecommendationStrategy();
-}
--- src/main/java/io/meli/melishop/strategy/RecommendationStrategy.java
+++
@@ -1,9 +0,0 @@
-package io.meli.melishop.strategy;
-
-import java.util.List;
-
-import io.meli.melishop.model.User;
-
-public interface RecommendationStrategy {
-    List<String> recommendProducts(User user);
-}
